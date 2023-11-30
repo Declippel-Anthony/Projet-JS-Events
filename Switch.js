9fb0eead8db63f05893e551951700a2ba8b6af36
Declippel-Anthony <anthony.declippel@limayrac.fr>
2023-11-30 14:33:06 +0100

    Ajout de la class Switch avec on code

---
+++ Switch.js
@@ -0,0 +1,31 @@
+class Switch {
+  positon;
+
+  constructor() {
+    this.off();
+  }
+
+  on() {
+    this.position = SwitchPosition.on;
+  }
+
+  off() {
+    this.position = SwitchPosition.off;
+  }
+
+  middle() {
+    this.position = SwitchPosition.middle;
+  }
+
+  isOn() {
+    if (this.position === SwitchPosition.on) return true;
+  }
+
+  isOff() {
+    if (this.position === SwitchPosition.off) return true;
+  }
+
+  isMiddlePosition() {
+    if (this.position === SwitchPosition.middle) return true;
+  }
+}
