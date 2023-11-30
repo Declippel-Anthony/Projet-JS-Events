a59bb18eddd1eea6681ea39a3bbebd473848dd06
Declippel-Anthony <anthony.declippel@limayrac.fr>
2023-11-30 15:39:56 +0100

    ajout du fichier CarUI avec le code du constructeur et la déclaration de la méthode operateDoor avec ses paramètres side et buttonElement

---
+++ CarUI.js
@@ -0,0 +1,9 @@
+class CarUI {
+  constructor() {
+    this.car = new Car();
+    //this.carPicture = new CarPicture;
+    //this.lightPicture = new LightPicture;
+  }
+
+  operateRightDoor(side, buttonElement) {}
+}
